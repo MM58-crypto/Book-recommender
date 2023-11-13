ae2d195684903b9b5651ca8223b7715f20cb038d
MM <mohammed.magdi999@gmail.com>
2023-11-13 14:13:05 +0300

    removed with st.form function

--- main.py
+++ main.py
@@ -12,37 +12,47 @@ st.write('Explore new books tailored just for you! Input your favorite genres or
 ## summary of webapp/ tiny description
 #load_dotenv(find_dotenv())
 #openai_api_key = os.getenv("OPENAI_API_KEY")
-with st.form('form1'):
+#with st.form('form1'):
 
-    st.subheader('Book type')
-    st.write('Select your preferred book type')
-    book_type = st.radio(
-            "Book type",
-            ["Non-fiction", "Fiction"]) 
+st.subheader('Book type')
+st.write('Select your preferred book type')
+    
+fiction_books =  ['sci-fi', 'fantasy', 'dystopian', 'mystery', 'horror', 'thriller', 'romance']
+nonfiction_books = ['Autobiograhy', 'Biography', 'Academic', 'Cookbook', 'Science', 'History', 'Travel', 'True crime', 'Art & photography']
+
+if 'selected_books' not in st.session_state:
+    st.session_state.selected_books = []
+
+book_type = st.radio(
+        "Book type",
+         ["Non-fiction", "Fiction"]) 
 
 # display different option box for non-fiction
 # if user selects non-fiction option --> display non-fiction options , (same for 
 ## fiction genres
-    if book_type == "Fiction":
+       
+if book_type == "Fiction":
 
-        st.subheader('Fiction Book Genre')
-        options = st.multiselect(
+    st.subheader('Fiction Book Genre')
+    st.session_state.selected_books= st.multiselect(
+            'Select up to five book genres that you prefer',
+               # ['sci-fi', 'fantasy', 'dystopian', 'mystery', 'horror', 'thriller', 'romance'] 
+            fiction_books, st.session_state.selected_books)
+
+elif book_type == "Non-fiction":
+        st.subheader('Non-fiction Books genre')
+        st.session_state.selected_books = st.multiselect(
                 'Select up to five book genres that you prefer',
-                ['Sci-Fi', 'Fantasy', 'Dystopian', 'Mystery', 'Horror', 'Thriller', 'Romance'])
-
-    if book_type == "Non-fiction":
-          st.subheader('Non-fiction Books genre')
-          options = st.multiselect(
-                   'Select up to five book genres that you prefer',
-                   ['Autobiograhy', 'Biography', 'Academic', 'Cookbook', 'Science', 'History', 'Travel', 'True crime', 'Art & photography'])
+                #   ['Autobiograhy', 'Biography', 'Academic', 'Cookbook', 'Science', 'History', 'Travel', 'True crime', 'Art & photography']
+                nonfiction_books, st.session_state.selected_books)
    
-    st.subheader("Number of pages")
+st.subheader("Number of pages")
     # + 200 pages
     # - 200 pages
     # 150 ~ 200 pages
-    pages = st.selectbox(
-            'How many pages do you want in your recommended book(s)?',
-            ('More than 200 pages', 'Less than 200 pages', 'Between 150 pages and 200 pages'))
+pages = st.selectbox(
+        'How many pages do you want in your recommended book(s)?',
+        ('More than 200 pages', 'Less than 200 pages', 'Between 150 pages and 200 pages'))
    
 
-    #submitted = st.form_submit_button('Generate') 
+   # submitted = st.form_submit_button('Generate') 
