0028c56ba70911727c1f04d830b0dc116acce5b0
MM <mohammed.magdi999@gmail.com>
2024-11-22 14:59:14 +0300

    new update

--- main.py
+++ main.py
@@ -1,82 +1,94 @@
 import streamlit as st
-import openai
+import google.generativeai as genai
 import os
 import config
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv, find_dotenv
 
 st.title('Book Recommender')
 st.divider()
 st.write('Explore new books tailored just for you! Input your favorite genres or books, and our Book Recommendation System will suggest your next great read. Enjoy')
 ## summary of webapp/ tiny description
-load_dotenv(find_dotenv())
-openai_api_key = os.getenv("OPENAI_API_KEY")
-def generate_response(input_text):
-    llm= OpenAI(temperature=0.7, openai_api_key=config.OPENAI_API_KEY)
-    st.success(llm(input_text))
 
-with st.form('form1'):
 
-    st.subheader('Book type')
-    st.write('Select your preferred book type')
+gemini_key = config.GEMINI_API_KEY
+genai.configure(api_key=gemini_key)
+model = genai.GenerativeModel("gemini-1.5-pro")
+
+tab1, tab2 = st.tabs(['Fiction Books Genre', ' Non-fiction Books Genre'])
+
+with tab1:
+
+    st.header('Fiction Books')
+    #st.write('Select your preferred book type')
     
     fiction_books =  ['sci-fi', 'fantasy', 'dystopian', 'mystery', 'horror', 'thriller', 'romance']
-    nonfiction_books = ['Autobiograhy', 'Biography', 'Academic', 'Cookbook', 'Science', 'History', 'Travel', 'True crime', 'Art & photography']
+    # allow the user to select multiple genres at once (most classics discuss various themes)
 
-  #  if 'selected_books' not in st.session_state:
-  #     st.session_state.selected_books = []
-
-    book_type = st.radio(
-            "Book type",
-            ["Non-fiction", "Fiction"]) 
-
-# display different option box for non-fiction
-# if user selects non-fiction option --> display non-fiction options , (same for 
-## fiction genres)
- 
-#book_type == "Fiction":
-       # fiction_genres = st.session_state.selected_books
-    st.caption("(Select only from the dropdown list of your preferred book type)")
-    st.subheader('Fiction Book Genre')
-    fiction_genres= st.selectbox(
+    st.subheader('Book Genre')
+    fiction_genres= st.multiselect(
                 'Select your preferred book genre',
                # ['sci-fi', 'fantasy', 'dystopian', 'mystery', 'horror', 'thriller', 'romance'] 
                 fiction_books)
- 
-   # ron_fiction_genres = st.session_state.selected_books
-    st.subheader('Non-fiction Books genre')
-    non_fiction_genres = st.selectbox(
-            'Select your preferred book genre',
-                #   ['Autobiograhy', 'Biography', 'Academic', 'Cookbook', 'Science', 'History', 'Travel', 'True crime', 'Art & photography']
-             nonfiction_books)
+    # give user the option to select the number of books to be recommended
+    st.subheader('Number of books to be recommended')
+    r_books = st.number_input("Number of books to recommend", min_value=1 , step=1)
+
     st.subheader("Number of pages")
     # + 200 pages
     # - 200 pages
     # 150 ~ 200 pages
-    pages = st.selectbox(
+    nf_pages = st.selectbox('How many pages should the book be?',
+                         ('More than 200 pages', 'Less than 200 pages' , 'Between 150 pages and 200 pages'))
+    fiction_prompt = f"Recommend a list of {r_books} fiction books with each boook containing the folowing genres: {fiction_genres} and being {nf_pages}"
+    
+
+    #prompt = PromptTemplate(input_variables=["booktype","n_pages", "genre"] ,
+    #                       template="Generate a list of ten {booktype} {genre} books with each book being {n_pages} ")
+    submitted = st.button('Generate Recommendations')
+    if submitted:
+        response = model.generate_content(fiction_prompt)
+        st.success(response.text)
+
+with tab2:
+    st.header('Non-fiction Books')
+
+    nonfiction_books = ['Autobiography', 'Biography', 'Academic', 'Cookbook', 'Science', 'History', 'Travel', 'True crime', 'Art & photography']
+
+    st.subheader('Book Genre')
+    nonfiction_genres = st.multiselect('Select your preferred book genre', nonfiction_books)
+
+    st.subheader('Number of books to be recommended')
+    rnf_books = st.number_input("Number of books", min_value=1 , step=1)
+
+    st.subheader("Number of pages")
+    f_pages = st.selectbox(
             'How many pages do you want in your recommended book(s)?',
             ('More than 200 pages', 'Less than 200 pages', 'Between 150 pages and 200 pages'))
-   
-    prompt = PromptTemplate(input_variables=["booktype","n_pages", "genre"] ,
-                           template="Generate a list of ten {booktype} {genre} books with each book being {n_pages} ")
-    submitted = st.form_submit_button('Generate')
-    if submitted:
-        if book_type == "Fiction":
-            generate_response(prompt.format(booktype=book_type, n_pages=pages, genre=fiction_genres))
-        elif book_type == "Non-fiction":
-            generate_response(prompt.format(booktype=book_type, n_pages=pages, genre=non_fiction_genres))
+
+    non_fiction_prompt = f"Recommend a list of {rnf_books} non-fiction books with each book containing the following genres:  {nonfiction_genres} being {f_pages}"
+    nf_submitted = st.button('Generate')
+
+    if nf_submitted:
+        response = model.generate_content(non_fiction_prompt)
+        st.success(response.text)
+
+
 # Book summarizer
 # (User enters the selected book in a text field and clicks a button. Once clicked, the sys will generate text that contains the summary of the selected book)
 with st.form('form2'):
     st.subheader('Book Summarizer')
     book_title = st.text_input('Enter your selected book title', '')
-    summary_prompt = PromptTemplate(input_variables=["booktitle"] ,
-                           template= "_Summarize the book {booktitle} in one paragraph")
+    summary_prompt = f"Summarize the book {book_title} in three paragraph"
+    #summary_prompt = PromptTemplate(input_variables=["booktitle"] ,
+    #                       template= "_Summarize the book {booktitle} in one paragraph")
     summarize_btn = st.form_submit_button("Summarize")
-
     if summarize_btn:
-        generate_response(summary_prompt.format(booktitle=book_title))
+        response = model.generate_content(summary_prompt)
+        st.success(response.text)
 
+# Need another feature (about the author?) , Discussion bot!
 
+with st.sidebar:
+    # Add a page for discussion bot
+    #st.subheader("This is a sidebar")
+    #st.page_link("main.py", label="Home")
+    st.page_link("pages/discussion_bot.py", label="Discussion Chatbot")
---
+++ pages/discussion_bot.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+import config
+import time
+
+st.title("Book Discussion bot!")
+st.caption("A chatbot for discussing books of your choice powered by Gemini!")
+
+gemini_key = config.GEMINI_API_KEY
+genai.configure(api_key=gemini_key)
+
+
+
+prompt_template = '''
+You are a helpful assistant with vast knowledge in Books. You have the ability to answer most if not all questions related to
+countless various successfull books (from many different genres) in a clear and detailed fashion.
+You only answer questions related to novels/books. (An important execption is holy books. No need to delve into them as you may make mistakes). Otherwise, end the conversation with this is not my domain
+You may respond to the user in arabic if the query is in the arabic language
+'''
+
+# Chat/Convo History
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+st.session_state.model =  genai.GenerativeModel("gemini-1.5-pro")
+st.session_state.chat = st.session_state.model.start_chat(
+    history = [ {"role": m["role"], "parts": m["content"]}
+                for m in st.session_state.messages
+               ]
+
+)
+# Display chat messages from history
+
+for message in st.session_state.messages:
+    with st.chat_message(
+            message["role"]):
+        st.markdown(message["content"])
+
+# user input
+
+if prompt :=  st.chat_input("Lets chat!" + prompt_template) :
+    # display user message in chat msg box
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # add user msgs to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt_template + '\n' + prompt})
+
+    # send msg to gemini assistant
+
+    response = st.session_state.chat.send_message(
+        prompt,
+        stream=True,
+    )
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ''
+
+        for chunk in response:
+            for ch in chunk.text.split(' '):
+                full_response +=ch + ' '
+                time.sleep(0.05)
+                message_placeholder.write(full_response + '▌')
+        # Write full message with placeholder
+
+        message_placeholder.write(full_response)
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": st.session_state.chat.history[-1].parts[0].text}
+    )
+
+
+#    with st.chat_message("assistant"):
+#        chat = model.start_chat(
+#            history=[
+#                {"role": m["role"], "parts": m["parts"]}
+#                for m in st.session_state.messages
+#            ]
+#        )
+#        gemini_response = chat.send_message(prompt, stream=True)
+#        response_stream = st.write_stream(gemini_response)
+#
+#    st.session_state.messages.append({"role": "assistant", "parts": response_stream})
+#'''
